6cd592c08c8be93bc7bfebee04ec328d79f89f15
heoseungyeon <dia0312@naver.com>
2021-10-27 08:57:59 +0900

    Feat: 좌석 등급 enum code 필드 추가

--- src/main/java/com/grepp/carrierroute/flight/domain/CabinClass.java
+++ src/main/java/com/grepp/carrierroute/flight/domain/CabinClass.java
@@ -1,7 +1,17 @@
 package com.grepp.carrierroute.flight.domain;
 
 public enum CabinClass {
-    ECONOMY,
-    BUSINESS,
-    FIRST
+    ECONOMY("CCC_001"),
+    BUSINESS("CCC_002"),
+    FIRST("CCC_003");
+
+    private final String cabinClassCode;
+
+    CabinClass(String cabinClassCode){
+        this.cabinClassCode = cabinClassCode;
+    }
+
+    public String getCode(){
+        return this.cabinClassCode;
+    }
 }
