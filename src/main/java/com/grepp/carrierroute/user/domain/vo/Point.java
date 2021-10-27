267e7fc59e0bdba49274901d9e5e1db9652ee966
Taehyun Lee <taehyun16@naver.com>
2021-10-27 16:31:14 +0900

    Feat: [#12] User 도메인 구현
    - User Entity 구현
    - User Repository 구현
    
    Feat: [#9]렌트카 조회 기능 구현
    
    - 렌트카 컨트롤러 구현
    - 렌트카 서비스 구현
    - 렌트카 repository 구현
    - 렌트카 dto 구현
    - 렌트카 converter 구현
    - 렌트카 entity 및 vo 구현
    - 취소 정책, 환불 정책 enum 생성

---
+++ src/main/java/com/grepp/carrierroute/user/domain/vo/Point.java
@@ -0,0 +1,13 @@
+package com.grepp.carrierroute.user.domain.vo;
+
+import lombok.Getter;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter
+public class Point {
+
+    private int point;
+
+}
