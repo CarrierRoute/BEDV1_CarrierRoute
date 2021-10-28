9f4726d55bb5abaac90e3bec57e83412c72f9de5
heoseungyeon <dia0312@naver.com>
2021-10-28 17:25:53 +0900

    Fix: findFlightsBy h2 query 수정

--- src/main/java/com/grepp/carrierroute/flight/repository/FlightRepository.java
+++ src/main/java/com/grepp/carrierroute/flight/repository/FlightRepository.java
@@ -1,6 +1,5 @@
 package com.grepp.carrierroute.flight.repository;
 
-import com.grepp.carrierroute.flight.domain.CabinClass;
 import com.grepp.carrierroute.flight.domain.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,12 +9,12 @@ import java.time.LocalDate;
 import java.util.List;
 
 public interface FlightRepository extends JpaRepository<Flight,Long> {
-    @Query(value = "select f.id from flight f left join flight_cabin_class fcc ON f.id = fcc.flight_id " +
-            "WHERE function(\"date_format\", f.departure_time, '%Y-%m-%d') = :departureDate AND " +
-            "f.departure_city = :departureCity AND " +
-            "f.arrival_city = :arrivalCity AND " +
-            "fcc.seat_cnt >= :headCount AND " +
-            "fcc.cabin_class = :cabinClass")
+    @Query(value = "select f.id from Flight f left join FlightCabinClass fcc ON f.id = fcc.flight.id " +
+            "WHERE function('date_format', f.departureDateTime, '%Y-%m-%d') = :departureDate AND " +
+            "f.departureCity = :departureCity AND " +
+            "f.arrivalCity = :arrivalCity AND " +
+            "fcc.seatCount >= :headCount AND " +
+            "fcc.cabinClass = :cabinClass")
     List<Long> findFlightsBy(@Param("departureCity")String departureCity, @Param("departureDate")LocalDate departureDate,
                              @Param("arrivalCity")String arrivalCity, @Param("headCount")Long headCount,
                              @Param("cabinClass")String cabinClass);
