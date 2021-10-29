20459bfb27cd7bfbb428e980685343e3441a0f36
Taehyun Lee <taehyun16@naver.com>
2021-10-29 21:47:24 +0900

    Feat: [#31] 렌트카 예약 관련 예외처리
    - Bean Validation
    - Domain에 제약조건 추가
    - Custom Exception 생성
    - ExceptionHandler 구현

--- src/main/java/com/grepp/carrierroute/booking/dto/CarBookingRequestDto.java
+++ src/main/java/com/grepp/carrierroute/booking/dto/CarBookingRequestDto.java
@@ -3,13 +3,19 @@ package com.grepp.carrierroute.booking.dto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
 @NoArgsConstructor
 @Getter
 public class CarBookingRequestDto {
 
+    @NotBlank
     private String carId;
+
+    @NotBlank
     private LocalDateTime startDateTime;
+
+    @NotBlank
     private LocalDateTime endDateTime;
 }
---
+++ src/main/java/com/grepp/carrierroute/booking/exception/LackOfPointException.java
@@ -0,0 +1,8 @@
+package com.grepp.carrierroute.booking.exception;
+
+public class LackOfPointException extends RuntimeException {
+
+    public LackOfPointException(String message) {
+        super(message);
+    }
+}
