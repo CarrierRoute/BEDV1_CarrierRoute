31b0ad06a5289fb459274fb915ba30de2930835c
nasaoreo <nasaoreo121@gmail.com>
2021-11-01 17:11:19 +0900

    Feat: Address, Point 빌더 메소드 추가

--- src/main/java/com/grepp/carrierroute/user/domain/vo/Point.java
+++ src/main/java/com/grepp/carrierroute/user/domain/vo/Point.java
@@ -1,15 +1,23 @@
 package com.grepp.carrierroute.user.domain.vo;
 
 import com.grepp.carrierroute.booking.exception.LackOfPointException;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
 
 @Embeddable
+@NoArgsConstructor
 @Getter
 public class Point {
 
     private long point;
+
+    @Builder
+    public Point(long point) {
+        this.point = point;
+    }
 
     public void subtract(long totalPrice){
         if (this.point < totalPrice) {
