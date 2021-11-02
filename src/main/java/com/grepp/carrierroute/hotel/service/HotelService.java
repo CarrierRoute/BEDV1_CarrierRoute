14716391733101fe5f798fa0a8ffd2968d4f0cc7
nasaoreo <nasaoreo121@gmail.com>
2021-11-02 14:59:32 +0900

    Refactor: 호텔 방 검색(findRoomBy) 메소드 위치 변경
    findRoomBy 메소드는 HotelRoomService에 위치하며 Hotel 정보를 이용하여 예약 가능한 호텔 방을 찾는 역할을 수행합니다.
    이는 Hotel 검색 시에만 사용되는 메소드이기 때문에 HotelRoomService 보다는 HoteService에 포함시키는 것이 더 적절할 것으로 판단하였습니다.
    따라서 HotelRoomService를 삭제하고 HotelService에 findRoomBy 메소드를 추가하였습니다.

--- src/main/java/com/grepp/carrierroute/hotel/service/HotelService.java
+++ src/main/java/com/grepp/carrierroute/hotel/service/HotelService.java
@@ -1,33 +1,37 @@
 package com.grepp.carrierroute.hotel.service;
 
+import com.grepp.carrierroute.booking.repository.HotelBookingRepository;
 import com.grepp.carrierroute.hotel.domain.Hotel;
 import com.grepp.carrierroute.hotel.domain.HotelRoom;
+import com.grepp.carrierroute.hotel.domain.RoomType;
 import com.grepp.carrierroute.hotel.dto.*;
 import com.grepp.carrierroute.hotel.exception.EmptyHotelInfoException;
 import com.grepp.carrierroute.hotel.exception.ErrorMessage;
 import com.grepp.carrierroute.hotel.exception.HotelInfoNotFoundedException;
 import com.grepp.carrierroute.hotel.repository.HotelRepository;
+import com.grepp.carrierroute.hotel.repository.HotelRoomRepository;
 import com.grepp.carrierroute.hotel.service.converter.HotelConverter;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class HotelService {
     private final HotelRepository hotelRepository;
-    private final HotelRoomService hotelRoomService;
+    private final HotelBookingRepository hotelBookingRepository;
     private final HotelConverter converter;
 
     public HotelSearchResponseDto findHotelBy(Long id, HotelSearchRequestDto requestDto){
         Hotel hotel = findHotelBy(id);
-        List<HotelRoom> roomsMatchedByRequest = hotelRoomService.findRoomsBy(hotel, requestDto);
+        List<HotelRoom> roomsMatchedByRequest = findRoomsBy(hotel, requestDto);
 
         if(roomsMatchedByRequest.isEmpty()){
             throw new EmptyHotelInfoException(ErrorMessage.HOTEL_NOT_FOUNDED_MATCHED_BY_REQUEST);
@@ -73,7 +77,7 @@ public class HotelService {
         List<HotelSearchResponseDto> hotelSearchResults = new ArrayList<>();
 
         findHotelsBy(requestDto.getDestinationType(), requestDto.getDestinationName()).forEach(hotel -> {
-            List<HotelRoom> roomsMatchedByRequest = hotelRoomService.findRoomsBy(hotel, requestDto);
+            List<HotelRoom> roomsMatchedByRequest = findRoomsBy(hotel, requestDto);
 
             if(!roomsMatchedByRequest.isEmpty()) {
                 hotelSearchResults.add(converter.convertToHotelSearchResponseDto(hotel, roomsMatchedByRequest));
@@ -81,5 +85,24 @@ public class HotelService {
         });
 
         return hotelSearchResults;
+    }
+
+    private List<HotelRoom> findRoomsBy(Hotel hotel, HotelSearchRequestDto requestDto){
+        Map<RoomType, List<HotelRoom>> availableRoomsByType = hotel.getHotelRooms()
+                .stream()
+                .filter(room -> !isRoomBooked(room.getId(), requestDto.getCheckInDate(), requestDto.getCheckOutDate()))
+                .collect(Collectors.groupingBy(HotelRoom::getRoomType));
+
+        return availableRoomsByType.keySet()
+                .stream()
+                .filter(roomType -> availableRoomsByType.get(roomType).size() >= requestDto.getNumOfRoom())
+                .map(availableRoomsByType::get)
+                .flatMap(List::stream)
+                .filter(room -> room.getMaxNumOfGuest() >= requestDto.getNumOfGuest())
+                .collect(Collectors.toList());
+    }
+
+    private boolean isRoomBooked(Long roomId, LocalDate checkInDate, LocalDate checkOutDate){
+        return hotelBookingRepository.existsByHotelRoomAndDate(roomId, checkInDate, checkOutDate);
     }
 }
