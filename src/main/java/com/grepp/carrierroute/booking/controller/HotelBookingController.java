3517c7f8d560ba0135cab8a32fd14532fc78aa20
nasaoreo <nasaoreo121@gmail.com>
2021-11-04 22:58:00 +0900

    Feat: 호텔 관련 Controller의 반환 타입 변경
    모든 controller 메소드가 ResponseEntity 객체 반환하도록 변경

--- src/main/java/com/grepp/carrierroute/booking/controller/HotelBookingController.java
+++ src/main/java/com/grepp/carrierroute/booking/controller/HotelBookingController.java
@@ -5,32 +5,43 @@ import com.grepp.carrierroute.booking.dto.HotelBookingRequestDto;
 import com.grepp.carrierroute.booking.dto.HotelBookingResponseDto;
 import com.grepp.carrierroute.booking.service.HotelBookingService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/")
 public class HotelBookingController {
     private final HotelBookingService hotelBookingService;
 
     @PostMapping("/bookings/hotels")
-    public HotelBookingResponseDto bookRooms(@RequestBody HotelBookingRequestDto bookingRequestDto, @CookieValue(value = "userId") String userId){
-        return hotelBookingService.bookRooms(bookingRequestDto, userId);
+    public ResponseEntity<HotelBookingResponseDto> bookRooms(@RequestBody HotelBookingRequestDto bookingRequestDto, @CookieValue(value = "userId") String userId){
+        return new ResponseEntity<>(hotelBookingService.bookRooms(bookingRequestDto, userId), HttpStatus.CREATED);
     }
 
     @GetMapping("/bookings/hotels")
-    public List<HotelBookingDetailsDto> getHotelBookings(@CookieValue(value = "userId") String userId){
-        return hotelBookingService.getHotelBookings(userId);
+    public ResponseEntity<List<HotelBookingDetailsDto>> getHotelBookings(@CookieValue(value = "userId") String userId){
+        List<HotelBookingDetailsDto> hotelBookings = hotelBookingService.getHotelBookings(userId);
+
+        if(hotelBookings.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(hotelBookings, HttpStatus.OK);
     }
 
     @GetMapping("/bookings/hotels/{bookingId}")
-    public HotelBookingDetailsDto getHotelBooking(@PathVariable Long bookingId){
-        return hotelBookingService.getHotelBooking(bookingId);
+    public ResponseEntity<HotelBookingDetailsDto> getHotelBooking(@PathVariable Long bookingId){
+        return new ResponseEntity<>(hotelBookingService.getHotelBooking(bookingId), HttpStatus.OK);
     }
 
     @DeleteMapping("/bookings/hotels/{bookingId}")
-    public void cancelHotelBooking(@PathVariable Long bookingId, @CookieValue(value = "userId") String userId){
+    public ResponseEntity<Long> cancelHotelBooking(@PathVariable Long bookingId, @CookieValue(value = "userId") String userId){
         hotelBookingService.cancelHotelBooking(bookingId, userId);
+        return new ResponseEntity<>(bookingId, HttpStatus.OK);
     }
 }
